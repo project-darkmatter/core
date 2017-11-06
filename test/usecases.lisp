21a19d91a323e83aaed1ff53273a3e5eaf19cac2
tamamu <tamamu.1r1s@gmail.com>
2017-11-06 17:21:39 +0900

    Add getResult usecase test

--- test/usecases.lisp
+++ test/usecases.lisp
@@ -9,7 +9,7 @@
 
 (diag "Usecase layer tests")
 
-(plan 1)
+(plan 2)
 
 (subtest "Testing eval usecase"
   (let* ((code "(+ 1 2 3)")
@@ -23,5 +23,44 @@
           (is-type task-result 'just)
           (is (bind task-result #'task-result-status) :success)
           (is (bind task-result #'task-result-value) 6))))))
+
+(subtest "Testing getResult usecase"
+  (let ((task1 (make-task
+                 (lambda ()
+                   (sleep 1)
+                   (make-task-result :value 2 :status :success))))
+        (task2 (let ((context (make-hash-table)))
+                 (make-task
+                   (lambda ()
+                     (setf (gethash :temporary context) "i got lost!")
+                     (sleep 3)
+                     (make-task-result :value "i'm late!" :status :failure))))))
+    (let ((ref1 (register-task task1))
+          (ref2 (register-task task2)))
+      (let ((result2-1 (usecase.get-result (task-reference-id task2))))
+        (is-type result2-1
+                 'usecase.get-result-result)
+        (is (usecase.get-result-result-status result2-1)
+            :running)
+        (is (gethash :temporary (usecase.get-result-result-context result2-1))
+            "i got lost!"))
+      (diag "Wait for task1 to finish...")
+      (sleep 1)
+      (let ((result1 (usecase.get-result (task-reference-id task1))))
+        (is-type result1
+                 'usecase.get-result-result)
+        (is (usecase.get-result-result-status result1)
+            :success)
+        (is (usecase.get-result-result-value result1)
+            2))
+      (diag "Wait for task2 to finish...")
+      (sleep 2)
+      (let ((result2-2 (usecase.get-result (task-reference-id task2))))
+        (is-type result2-2
+                 'usecase.get-result-result)
+        (is (usecase.get-result-result-status result-2-2)
+            :failure)
+        (is (gethash :temporary (usecase.get-result-result-context result2-2))
+            "i got lost!")))))
 
 (finalize)
