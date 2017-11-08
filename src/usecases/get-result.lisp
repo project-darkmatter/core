e78c266e5ce688084223a3c34c4457c5f6097b15
tamamu <tamamu.1r1s@gmail.com>
2017-11-08 12:00:08 +0900

    Implement usecase.get-result

--- src/usecases/get-result.lisp
+++ src/usecases/get-result.lisp
@@ -4,23 +4,46 @@
         :darkmatter/utils/maybe
         :darkmatter/usecases/base)
   (:import-from :darkmatter/domains/task
-                :task-id
-                :make-task
-                :make-task-result
-                :register-task)
+                :retrieve-task
+                :task-result
+                :task-result-context
+                :task-result-status
+                :task-result-value
+                :try-get-task-result)
   (:export :usecase.get-result.result
-           :usecase.get-result.result-context
            :usecase.get-result.result-status
-           :usecase.get-result.result-value
+           :usecase.get-result.result-content
+           :usecase.get-result.result-content-context
+           :usecase.get-result.result-content-status
+           :usecase.get-result.result-content-value
            :usecase.get-result))
 (in-package :darkmatter/usecases/get-result)
 
+(deftype content-context () 'maybe)
+(deftype content-status () '(member :success :failure :running))
+(defstruct usecase.get-result.result-content
+  (context nil :type content-context)
+  (status nil :type content-status)
+  (value nil))
+
+(deftype usecase.get-result.result-status () 'boolean)
+(deftype usecase.get-result.%result-content () 'maybe)
 (defstruct usecase.get-result.result
-  context
-  status
-  value
-  )
+  (status nil :type usecase.get-result.result-status)
+  (content nil :type usecase.get-result.%result-content))
 
 (defun usecase.get-result (task-id)
-  )
+  (let ((task (retrieve-task task-id)))
+    (if task
+        (let ((task-result (try-get-task-result task)))
+          (make-usecase.get-result.result
+            :status t
+            :content (just
+                       (make-usecase.get-result.result-content
+                         :context (task-result-context task-result)
+                         :status (task-result-status task-result)
+                         :value (task-result-value task-result)))))
+        (make-usecase.get-result.result
+          :status nil
+          :content (nothing)))))
 
