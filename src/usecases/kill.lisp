0759ada86151e0303d156f07210705373467268c
tamamu <tamamu.1r1s@gmail.com>
2017-11-08 12:50:47 +0900

    Implement kill usecase

--- src/usecases/kill.lisp
+++ src/usecases/kill.lisp
@@ -8,13 +8,25 @@
                 :task-result-context
                 :cancel-task)
   (:export :usecase.kill.result
+           :usecase.kill.result-status
            :usecase.kill.result-context
            :usecase.kill))
 (in-package :darkmatter/usecases/kill)
 
 
+(deftype usecase.kill.result-status () 'boolean)
+(deftype usecase.kill.result-context () 'maybe)
 (defstruct usecase.kill.result
-  context)
+  (status nil :type usecase.kill.result-status)
+  (context nil :type usecase.kill.result-context))
 
 (defun usecase.kill (task-id)
-  )
+  (let ((task (retrieve-task task-id)))
+    (if task
+        (make-usecase.kill.result
+          :status t
+          :context (task-result-context
+                     (cancel-task task)))
+        (make-usecase.kill.result
+          :status nil
+          :context (nothing)))))
