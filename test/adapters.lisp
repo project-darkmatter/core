34e85e10e20c6daa494d7176aa87d6d5c2014c63
tamamu <tamamu.1r1s@gmail.com>
2017-11-09 23:49:53 +0900

    Remove a test which could not test yet, and implement getResult presenter

--- test/adapters.lisp
+++ test/adapters.lisp
@@ -7,7 +7,7 @@
         :darkmatter/usecases/get-result
         :darkmatter/usecases/kill
         :darkmatter/adapters/presenters/eval
-        ;:darkmatter/adapters/presenters/get-result
+        :darkmatter/adapters/presenters/get-result
         ;:darkmatter/adapters/presenters/kill
         :prove))
 (in-package darkmatter/test/adapters)
@@ -19,22 +19,18 @@
 
 (let ((id1 nil)
       (id2 nil)
-      (id3 nil)
-      (id4 nil))
+      (id3 nil))
 
   (subtest "Testing eval presenter"
     (let* ((code1 "nil")
            (code2 "(princ 42)")
-           (code3 "(setf (gethash :temporary %context%))(sleep 1000)")
-           (code4 "(error \"ERROR\")")
+           (code3 "(setf (gethash :|temporary| %context%) \"darkmatter\")(sleep 1000)")
            (result1 (usecase.eval code1))
            (result2 (usecase.eval code2))
-           (result3 (usecase.eval code3))
-           (result4 (usecase.eval code4)))
+           (result3 (usecase.eval code3)))
       (setf id1 (usecase.eval.result-task-id result1)
             id2 (usecase.eval.result-task-id result2)
-            id3 (usecase.eval.result-task-id result3)
-            id4 (usecase.eval.result-task-id result4))
+            id3 (usecase.eval.result-task-id result3))
       (flet ((result-format (id)
                (format nil "{\"taskId\":~A}" id)))
         (is (presenter.eval result1)
@@ -42,33 +38,27 @@
         (is (presenter.eval result2)
             (result-format id2))
         (is (presenter.eval result3)
-            (result-format id3))
-        (is (presenter.eval result4)
-            (result-format id4)))))
+            (result-format id3)))))
 
   (subtest "Testing getResult presenter"
     (let ((result1 (usecase.get-result id1))
           (result2 (usecase.get-result id2))
           (result3 (usecase.get-result id3))
-          (result4 (usecase.get-result id4))
-          (result5 (usecase.get-result -1)))
+          (result4 (usecase.get-result -1)))
       (is (presenter.get-result result1)
-          "{\"status\":true,\"content\":{\"status\":\"SUCCESS\",\"value\":null,\"output\":\"\",\"context\":null}}")
+          "{\"status\":true,\"content\":{\"status\":\"SUCCESS\",\"value\":null,\"output\":\"\",\"context\":{}}}")
       (is (presenter.get-result result2)
-          "{\"status\":true,\"content\":{\"status\":\"SUCCESS\",\"value\":42,\"output\":\"42\",\"context\":null}}")
+          "{\"status\":true,\"content\":{\"status\":\"SUCCESS\",\"value\":42,\"output\":\"42\",\"context\":{}}}")
       (is (presenter.get-result result3)
           "{\"status\":true,\"content\":{\"status\":\"RUNNING\",\"value\":null,\"output\":\"\",\"context\":{\"temporary\":\"darkmatter\"}}}")
       (is (presenter.get-result result4)
-          "{\"status\":true,\"content\":{\"status\":\"FAILURE\",\"value\":null,\"output\":\"\",\"context\":null}}")
-      (is (presenter.get-result result5)
           "{\"status\":false,\"content\":null}")))
 
   (subtest "Testing kill presenter"
     (let ((result1 (usecase.kill id1))
           (result2 (usecase.kill id2))
           (result3 (usecase.kill id3))
-          (result4 (usecase.kill id4))
-          (result5 (usecase.kill -1)))
+          (result4 (usecase.kill -1)))
       (is (presenter.kill result1)
           "{\"status\":true,\"context\":null}")
       (is (presenter.kill result2)
@@ -76,8 +66,6 @@
       (is (presenter.kill result3)
           "{\"status\":true,\"context\":{\"temporary\":\"darkmatter\"}")
       (is (presenter.kill result4)
-          "{\"status\":true,\"context\":null}")
-      (is (presenter.kill result5)
           "{\"status\":false,\"context\":null}"))))
 
 (finalize)
