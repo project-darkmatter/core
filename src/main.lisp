8e3e19b43e1fa0d40e1595a3de3fd12a63ae1e2c
tamamu <tamamu.1r1s@gmail.com>
2017-12-14 17:19:33 +0900

    Support output to log file

--- src/main.lisp
+++ src/main.lisp
@@ -20,8 +20,41 @@
       (jsonrpc:expose *server* (car api) (cdr api)))
     +apis+))
 
-(defun start (&key port)
+(defun start (&key (port 50000) log)
   (%register-apis)
-  (when (null port)
-    (setf port 50000))
-  (jsonrpc:server-listen *server* :port port :mode :websocket))
+  (let ((logfile-p nil))
+    (block nil
+      (unwind-protect
+        (progn
+          (if log
+              (if (probe-file log)
+                  (progn
+                    (logger:alert t "Log file ~A already exists!" (truename log))
+                    (return))
+                  (progn
+                    (setf logger:*log-output* (open log :direction :output)
+                          logfile-p t)
+                    (logger:normal t "Start Darkmatter with logging: ~A~%" (truename log))))
+              (setf logger:*log-output* *standard-output*))
+          (tagbody
+            start
+              (handler-case
+                (progn
+                  (format logger:*log-output* "#~A~%" port)
+                  (force-output logger:*log-output*)
+                  (jsonrpc:server-listen *server* :port port :mode :websocket :silent t))
+                ((or usocket:address-in-use-error error) (c)
+                  (incf port)
+                  (if (< 65535 port)
+                      (progn
+                        (logger:alert t "Connection failed, exit.")
+                        (format logger:*log-output* "#EXIT~%")
+                        (force-output logger:*log-output*)
+                        (go finish))
+                      (progn
+                        (logger:alert t "Connection failed, retry as port ~A" port)
+                        (format logger:*log-output* "#FAILURE~%")
+                        (force-output logger:*log-output*)
+                        (go start)))))
+            finish))
+        (if logfile-p (close logger:*log-output*))))))
