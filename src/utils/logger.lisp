155b92d3c42aac8c8450861156f1ecb9a2a90c3c
tamamu <tamamu.1r1s@gmail.com>
2018-01-24 12:04:18 +0900

    Modify to use no color when output stream is file stream

--- src/utils/logger.lisp
+++ src/utils/logger.lisp
@@ -14,19 +14,29 @@
        (second minute hour date month year day-of-week dst-p tz)
        (get-decoded-time)
        (declare (ignore date month year day-of-week dst-p tz))
-     (format ,stream (format nil "~C[32;1m[~2,'0d:~2,'0d:~2,'0d]~C[0m~A ~A~%"
-                            ,(code-char #o33) hour minute second ,(code-char #o33)
-                            ""
-                            (format nil ,formatter ,@rest)))
-    (force-output)))
+     (let ((*print-pretty* nil))
+       (if (typep ,stream 'file-stream)
+           (format ,stream (format nil "[~2,'0d:~2,'0d:~2,0d]~A ~A~%"
+                                   hour minute second ""
+                                   (format nil ,formatter ,@rest)))
+           (format ,stream (format nil "~C[32;1m[~2,'0d:~2,'0d:~2,'0d]~C[0m~A ~A~%"
+                                   ,(code-char #o33) hour minute second ,(code-char #o33)
+                                   ""
+                                   (format nil ,formatter ,@rest))))
+       (force-output))))
 
 (defmacro alert (stream formatter &rest rest)
   `(multiple-value-bind
        (second minute hour date month year day-of-week dst-p tz)
        (get-decoded-time)
        (declare (ignore date month year day-of-week dst-p tz))
-     (format ,stream (format nil "~C[31;1m[~2,'0d:~2,'0d:~2,'0d]~C[0m~A ~A~%"
-                            ,(code-char #o33) hour minute second ,(code-char #o33)
-                            ""
-                            (format nil ,formatter ,@rest)))
-    (force-output)))
+     (let ((*print-pretty* nil))
+       (if (typep ,stream 'file-stream)
+           (format ,stream (format nil "[~2,'0d:~2,'0d:~2,0d]~A ~A~%"
+                                   hour minute second ""
+                                   (format nil ,formatter ,@rest)))
+           (format ,stream (format nil "~C[31;1m[~2,'0d:~2,'0d:~2,'0d]~C[0m~A ~A~%"
+                                   ,(code-char #o33) hour minute second ,(code-char #o33)
+                                   ""
+                                   (format nil ,formatter ,@rest))))
+       (force-output))))
